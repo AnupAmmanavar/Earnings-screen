85a219e0e11b9fe322255afcb95dc4b9cc1ca507
AnupPorter <anup.a@theporter.in>
2020-05-20 14:29:28 +0530

    First commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id(BuildPlugins.androidApplication)
+    kotlin("android")
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExt)
+    id(BuildPlugins.kotlinKapt)
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(App.compileSdkVersion)
+
+    defaultConfig {
+        applicationId = "com.kinley.earnings"
+        minSdkVersion(App.minSdkVersion)
+        targetSdkVersion(App.targetSdkVersion)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+
+dependencies {
+    implementation(BuildPlugins.kotlinStdLib)
+    implementation(Libs.appCompat)
+    implementation(Libs.coreKtx)
+    implementation(Libs.constraintLayout)
+    implementation(Libs.epoxy)
+    annotationProcessor(Libs.epoxyProcessor)
+
+    testImplementation(TestLibs.junit)
+
+    androidTestImplementation(InstrumentationTestLibs.junit)
+    androidTestImplementation(InstrumentationTestLibs.espressoCore)
+}
