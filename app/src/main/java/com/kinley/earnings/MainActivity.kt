b6d221a72880a766a2d70b3ee760d6ca4a8e95bc
AnupPorter <anup.a@theporter.in>
2020-05-21 18:54:56 +0530

    Moved the view creation to separate functions

--- app/src/main/java/com/kinley/earnings/MainActivity.kt
+++ app/src/main/java/com/kinley/earnings/MainActivity.kt
@@ -6,17 +6,21 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.airbnb.epoxy.EpoxyController
+import com.airbnb.epoxy.EpoxyModel
 import com.kinley.ui.EarningsBindingModel_
 import com.kinley.ui.WeekBindingModel_
 import com.kinley.ui.WeekdayBindingModel_
+import com.kinley.ui.weekcomponent.WeekUIDelegate
 import com.kinley.ui.weekcomponent.WeekUiModel
-import com.kinley.ui.weekcomponent.WeekVMDelegate
+import com.kinley.ui.weekdaycomponent.WeekdayUIDelegate
+import com.kinley.ui.weekdaycomponent.WeekdayUiModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(), WeekVMDelegate {
+class MainActivity : AppCompatActivity(), WeekUIDelegate, WeekdayUIDelegate {
 
     private val vm: MainViewModel by viewModels()
 
@@ -27,46 +31,13 @@ class MainActivity : AppCompatActivity(), WeekVMDelegate {
         lifecycle.addObserver(vm)
 
         // Defining the view of weeks
-        rv_weeks.withModels {
-            val list = vm.uiState.weeksUiModel.value
-
-            list.map {
-                WeekBindingModel_()
-                    .id(it.id)
-                    .weekUiModel(it)
-                    .onClick { _ ->
-                        onWeekSelected(it)
-                    }
-                    .addTo(this)
-            }
-
-        }
+        rv_weeks.withModels { createWeekModels(this@MainActivity).render(this) }
 
         // Defining the view of weekdays
-        rv_weekdays.withModels {
-            val list = vm.uiState.daysUiModel.value
+        rv_weekdays.withModels { createWeekdaysModels(this@MainActivity).render(this) }
 
-            list.map {
-                WeekdayBindingModel_()
-                    .id(it.id)
-                    .weekdayUiModel(it)
-                    .onClick { v ->
-                        vm.updateDay(it.id)
-                    }
-                    .addTo(this)
-            }
-        }
-
-        rv_earnings.withModels {
-            val list = vm.uiState.earningsUiModel.value
-
-            list.map {
-                EarningsBindingModel_()
-                    .id(it.id)
-                    .uiModel(it)
-                    .addTo(this)
-            }
-        }
+        // Defining the views of earnings
+        rv_earnings.withModels { createEarningsModels().render(this) }
 
 
         // Observe for the changes in each of the components and re-build their UI
@@ -81,6 +52,55 @@ class MainActivity : AppCompatActivity(), WeekVMDelegate {
     override fun onWeekSelected(week: WeekUiModel) {
         vm.updateWeek(week.id)
     }
+
+    override fun onWeekdaySelected(weekdayUModel: WeekdayUiModel) {
+        vm.updateDay(weekdayUModel.id)
+    }
+
+
+    // ENHANCEMENT : Move the creation of Views outside of this class
+    private fun createWeekModels(uiDelegate: WeekUIDelegate): List<EpoxyModel<*>> {
+        val list = vm.uiState.weeksUiModel.value
+
+        return list.map {
+            WeekBindingModel_()
+                .id(it.id)
+                .weekUiModel(it)
+                .onClick { _ ->
+                    uiDelegate.onWeekSelected(it)
+                }
+        }
+
+    }
+
+    private fun createWeekdaysModels(uiDelegate: WeekdayUIDelegate): List<EpoxyModel<*>> {
+        val list = vm.uiState.daysUiModel.value
+
+        return list.map {
+            WeekdayBindingModel_()
+                .id(it.id)
+                .weekdayUiModel(it)
+                .onClick { v ->
+                    uiDelegate.onWeekdaySelected(it)
+                }
+
+        }
+    }
+
+    private fun createEarningsModels(): List<EpoxyModel<*>> {
+        val list = vm.uiState.earningsUiModel.value
+
+        return list.map {
+            EarningsBindingModel_()
+                .id(it.id)
+                .uiModel(it)
+
+        }
+    }
+}
+
+fun <T : EpoxyModel<*>> List<T>.render(controller: EpoxyController) {
+    this.forEach { it.addTo(controller) }
 }
 
 
